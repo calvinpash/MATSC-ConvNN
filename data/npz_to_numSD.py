2841af1e6ac7c80cba852e98dfc13d6218f328be
calvinpash <cpp5317@psu.edu>
2021-04-14 20:20:03 -0400

    Fixed npz so it slices by z-layer

--- data/npz_to_numSD.py
+++ data/npz_to_numSD.py
@@ -14,24 +14,26 @@ def main(args):
     from os import listdir
     sd = 2
     for arg in args:#Takes in command line arguments; less sugar but more rigorous
-        try: arg = int(arg)
+        try: arg = double(arg)
         except: pass
-        if type(arg) == int:
+        if type(arg) == double:
             sd = arg
         else:
             print(f"Argument '%s' ignored" % str(arg))
 
     all_dat = np.array([np.array(list(np.load(f"data/interim/{i}").values())) for i in os.listdir("data/interim")])
+    all_dat = np.moveaxis(all_dat[:,3,:,:,:],3,1)#allows us to iterate by z-layer
 
-    stress_mean = np.mean(all_dat[:,3,:,:,:])
-    stress_sd = np.std(all_dat[:,3,:,:,:])
+    stress_mean = np.mean(all_dat)
+    stress_sd = np.std(all_dat)
     #stress_by_layers = all_dat[:,3,:,:,:].reshape()
 
     shape_by_layer = (all_dat.shape[2]*all_dat.shape[0],all_dat.shape[3], all_dat.shape[4])
-    stress_by_layer = all_dat[:,3,:,:,:].reshape(shape_by_layer)
+    stress_by_layer = all_dat.reshape(shape_by_layer)
 
     stress_num_hotspot = np.array([np.sum(np.logical_or(layer < stress_mean - sd*stress_sd, stress_mean + sd*stress_sd < layer)) for layer in stress_by_layer])
 
+    print(f"Sigma: {sd}")
     print(f"\tMean\tSD")
     print(f"\t%.2f\t%.2f" % (np.mean(stress_num_hotspot), np.std(stress_num_hotspot)))
     #ori_by_layers = all_dat[:,:3,:,:,:].reshape(all_dat.shape[2]*all_dat.shape[0],all_dat.shape[3], all_dat.shape[4],3)
